3c7b9e7e5cf7b5a062c4c72d9d6ab795b1faef97
pexiep <74528809+pexiep@users.noreply.github.com>
2021-07-25 19:17:36 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "aws" {
   access_key = var.access_key
   secret_key = var.secret_key
 }
-
+####CREATE 2 VPC for PROD and DEV
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
@@ -45,3 +45,50 @@ module "vpc1" {
   
   }
 
+  #### CREATE EKS for 2 VPC
+  
+data "aws_eks_cluster" "cluster" {
+  name = module.my-cluster.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.my-cluster.cluster_id
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+  version                = "~> 1.9"
+}
+
+module "my-prod-env" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = "my-eks-prod-env"
+  cluster_version = "1.17"
+  subnets         = ["subnet-005f89dbe623cc8ea", "subnet-0ce5ea0073394d88c"]
+  vpc_id          = "vpc-0b16574d98c11e5fc"
+
+  worker_groups = [
+    {
+      instance_type = "m2.micro"
+      asg_max_size  = 3
+    }
+  ]
+}
+  
+ module "my-dev-env" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = "my-eks-dev-env"
+  cluster_version = "1.17"
+  subnets         = ["subnet-0ccdcf2498f1ea2d8", "subnet-0948a6444ac4dd85c"]
+  vpc_id          = "vpc-02f1976631032a0e6"
+
+  worker_groups = [
+    {
+      instance_type = "m2.micro"
+      asg_max_size  = 3
+    }
+  ]
+}
