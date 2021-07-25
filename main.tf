8c89902629bfc21a1f96146ff13e6f0a193d7d6f
pexiep <74528809+pexiep@users.noreply.github.com>
2021-07-25 19:05:52 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ module "vpc" {
   name = "PROD_ENV"
   cidr = "10.0.0.0/16"
 
-  azs             = ["eu-west-1a", "eu-west-1b"]
+  azs             = ["us-east-1a", "us-east-1b"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
@@ -36,7 +36,7 @@ module "vpc1" {
   name = "DEV_ENV"
   cidr = "172.31.0.0/16"
 
-  azs             = ["eu-west-1a", "eu-west-1b"]
+  azs             = ["us-east-1a", "us-east-1b"]
   private_subnets = ["172.31.1.0/24", "172.31.2.0/24"]
   public_subnets  = ["172.31.101.0/24","172.31.102.0/24"]
 
