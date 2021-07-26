29d31d6495c4034b9807c5aac0bbe0a6a23f4eac
pexiep <74528809+pexiep@users.noreply.github.com>
2021-07-26 21:26:54 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,11 +17,11 @@ provider "aws" {
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "PROD_ENV"
-  cidr = "10.0.0.0/16"
+  name = "PROD"
+  cidr = "10.2.0.0/16"
 
-  azs             = ["us-east-1a", "us-east-1b"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", ]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
   enable_nat_gateway = true
