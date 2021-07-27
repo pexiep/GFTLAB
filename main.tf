6a83f34a5a4235259cd97fe8bf20c5ffbba199cf
pexiep <74528809+pexiep@users.noreply.github.com>
2021-07-27 21:41:13 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,31 +20,52 @@ module "vpc" {
   name = "PROD"
   cidr = "10.2.0.0/16"
 
-  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", ]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+  azs                 = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  private_subnets     = ["10.2.10.0/24", "10.2.11.0/24", "10.2.22.0/24"]
+  public_subnets      = ["10.2.0.0/24", "10.2.1.0/24", "10.2.2.0/24"]
+  database_subnets    = ["10.2.100.0/24", "10.2.110.0/24", "10.2.220.0/24"]
 
   enable_nat_gateway = true
   enable_vpn_gateway = true
-  
+  one_nat_gateway_per_az = false
   }
 
 
 module "vpc1" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "DEV_ENV"
-  cidr = "172.31.0.0/16"
+  name = "NON-PROD"
+  cidr = "10.1.0.0/16"
 
-  azs             = ["us-east-1a", "us-east-1b"]
-  private_subnets = ["172.31.1.0/24", "172.31.2.0/24"]
-  public_subnets  = ["172.31.101.0/24","172.31.102.0/24"]
+  azs                 = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  private_subnets     = ["10.1.10.0/24", "10.1.11.0/24", "10.1.22.0/24"]
+  public_subnets      = ["10.1.0.0/24","10.1.1.0/24", "10.1.2.0/24"]
+  database_subnets    = ["10.1.100.0/24", "10.1.110.0/24", "10.1.220.0/24"]
 
   enable_nat_gateway = true
   enable_vpn_gateway = true
-  
+  one_nat_gateway_per_az = false
   }
 
+  
+module "eks" {
+  source       = "git::https://github.com/terraform-aws-modules/terraform-aws-eks.git?ref=v12.1.0"
+  cluster_name = local.cluster_name
+  vpc_id       = module.vpc.aws_vpc_id
+  subnets      = module.vpc.aws_subnet_private_prod_ids
+
+  node_groups = {
+    eks_nodes = {
+      desired_capacity = 3
+      max_capacity     = 3
+      min_capaicty     = 3
+
+      instance_type = "t2.small"
+    }
+  }
+
+  manage_aws_auth = false
+}
   #### CREATE EKS for 2 VPC
 ###Code 1
 provider "aws" {
