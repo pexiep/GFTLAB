6686b2a74e10e33f326619fcf2b1a6f0b128c8af
pexiep <74528809+pexiep@users.noreply.github.com>
2021-08-01 12:05:12 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,6 +14,7 @@ provider "aws" {
   secret_key = var.secret_key
 }
 ####CREATE 2 VPC for PROD and DEV
+
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
@@ -25,11 +26,15 @@ module "vpc" {
   public_subnets      = ["10.2.0.0/24", "10.2.1.0/24", "10.2.2.0/24"]
   database_subnets    = ["10.2.100.0/24", "10.2.110.0/24", "10.2.220.0/24"]
 
-  enable_nat_gateway = true
-  enable_vpn_gateway = true
-  one_nat_gateway_per_az = false
+  enable_nat_gateway  = true
+  single_nat_gateway  = true
   }
+  
+resource "aws_eip" "nat" {
+  count = 1
 
+  vpc = true
+}
 
 module "vpc1" {
   source = "terraform-aws-modules/vpc/aws"
@@ -42,9 +47,8 @@ module "vpc1" {
   public_subnets      = ["10.1.0.0/24","10.1.1.0/24", "10.1.2.0/24"]
   database_subnets    = ["10.1.100.0/24", "10.1.110.0/24", "10.1.220.0/24"]
 
-  enable_nat_gateway = true
-  enable_vpn_gateway = true
-  one_nat_gateway_per_az = false
+  enable_nat_gateway  = true
+  single_nat_gateway  = true
   }
 
  
