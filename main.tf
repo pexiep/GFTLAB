b50e066f2482d3cfc39f934ca799afaebe8dcc6e
pexiep <74528809+pexiep@users.noreply.github.com>
2021-08-01 16:09:08 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,6 +30,20 @@ module "vpc" {
   single_nat_gateway  = true
   }
  
+  module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "NON-PROD"
+  cidr = "10.1.0.0/16"
+
+  azs                 = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  private_subnets     = ["10.1.10.0/24", "10.1.11.0/24", "10.1.22.0/24"]
+  public_subnets      = ["10.1.0.0/24","10.1.1.0/24", "10.1.2.0/24"]
+  database_subnets    = ["10.1.100.0/24", "10.1.110.0/24", "10.1.220.0/24"]
+
+  enable_nat_gateway  = true
+  single_nat_gateway  = true
+  }
  ###EKS Creation
   
   module "eks" {
